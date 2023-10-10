001038925854d72453c122c0951493ed2727c355
SaumickPradhan <saumick.pradhan@gmail.com>
2023-10-10 19:26:45 -0400

    font size changes

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import React, { Component } from 'react';
+import { useState } from "react";
+import React, { Component } from "react";
 
-import './App.css'
-import LeftVertical from './components/LeftVertical';
-import RightVertical from './components/RightVertical';
-import TopHorizontal from './components/TopHorizontal';
-import Middle from './components/Middle';
-import NotesPopup from './components/NotesPopup.jsx';
-import StandUpReminder from './components/StandUpReminder';
-import MusicPopup from './components/MusicPopup.jsx';
-import smartTable from './assets/smartTable.jpeg';
-import googleCal from './assets/googleCal.png';
-import outlookCal from './assets/outlookCal.png';
-import appleCal from './assets/appleCal.png';
-import dailyCal from './assets/dailyCal.png';
+import "./App.css";
+import LeftVertical from "./components/LeftVertical";
+import RightVertical from "./components/RightVertical";
+import TopHorizontal from "./components/TopHorizontal";
+import Middle from "./components/Middle";
+import NotesPopup from "./components/NotesPopup.jsx";
+import StandUpReminder from "./components/StandUpReminder";
+import MusicPopup from "./components/MusicPopup.jsx";
+import smartTable from "./assets/smartTable.jpeg";
+import googleCal from "./assets/googleCal.png";
+import outlookCal from "./assets/outlookCal.png";
+import appleCal from "./assets/appleCal.png";
+import dailyCal from "./assets/dailyCal.png";
 
-
-import phone from './assets/phone.jpeg';
-import watch from './assets/watch.jpeg';
-import airpods from './assets/airpods.jpeg';
-import stand from './assets/stand.png';
-import iMessagePic from './assets/imessage.png';
-import notes from './assets/notes.png';
-import clock from './assets/clock.gif';
-import music from './assets/music.png';
-import emailPic from './assets/email.png';
-import instagramPic from './assets/instagram.png';
-import slackPic from './assets/slack.png';
-import news from './assets/news.png';
+import phone from "./assets/phone.jpeg";
+import watch from "./assets/watch.jpeg";
+import airpods from "./assets/airpods.jpeg";
+import stand from "./assets/stand.png";
+import iMessagePic from "./assets/imessage.png";
+import notes from "./assets/notes.png";
+import clock from "./assets/clock.gif";
+import music from "./assets/music.png";
+import emailPic from "./assets/email.png";
+import instagramPic from "./assets/instagram.png";
+import slackPic from "./assets/slack.png";
+import news from "./assets/news.png";
 
 let defaultNews = [
   "News Update\nBreaking News: Scientists Discover New Species in the Amazon Rainforest",
@@ -39,7 +38,7 @@ let defaultNews = [
 
 let defaultUser = "Guest";
 
-let defaultIcon = news; 
+let defaultIcon = news;
 
 let defaultHeight = 50;
 
@@ -50,40 +49,36 @@ let defaultNotes = "Apple Notes";
 let defaultStand = "60";
 
 function App() {
-
   let [device, setDevice] = useState(null);
 
   let handleUserClick = (userValue) => {
     setDevice(userValue);
   };
 
-
-  
-
   const Email = [
     "        Email from Shubhra\nHello, Please send the required documents for this deliverable.",
     "Email from Samuel\nHey! It was nice to see you today!",
-    "Email from Tom\nHello, I am pleased to announce that you are selected for the next round of interviews."
+    "Email from Tom\nHello, I am pleased to announce that you are selected for the next round of interviews.",
   ];
-  
+
   const iMessage = [
     "iMessage from Cody\nHey, I am on my way.",
     "iMessage from Smith\nWhen can we meet? I want to share something!",
-    "iMessage from Brayden\nI am upstairs, let's hang out after you are done with your meeting."
+    "iMessage from Brayden\nI am upstairs, let's hang out after you are done with your meeting.",
   ];
-  
+
   const Instagram = [
     "Instagram Message from Cody\nCody sent a Reel from UofCincy.",
     "Instagram from Cory\nWhen can we meet? I want to share something!",
-    "Instagram from Brayden\nI am upstairs, let's hang out after you are done with your meeting."
+    "Instagram from Brayden\nI am upstairs, let's hang out after you are done with your meeting.",
   ];
-  
+
   const Slack = [
     "Slack from Jordan\nHey, I am on my way.",
     "Slack from Cory\nWhen can we meet? I want to share something!",
-    "Slack from Robert\nI am upstairs, let's hang out after you are done with your meeting."
+    "Slack from Robert\nI am upstairs, let's hang out after you are done with your meeting.",
   ];
-  
+
   const user1 = {
     id: 1,
     name: "Henry",
@@ -94,7 +89,7 @@ function App() {
     notesPreference: "Apple Notes",
     standupPreference: "60",
   };
-  
+
   const user2 = {
     id: 2,
     name: "Nancy",
@@ -105,7 +100,7 @@ function App() {
     notesPreference: "OneNote",
     standupPreference: "30",
   };
-  
+
   const user3 = {
     id: 3,
     name: "Donald",
@@ -116,7 +111,7 @@ function App() {
     notesPreference: "Notion",
     standupPreference: "90",
   };
-  
+
   const user4 = {
     id: 4,
     name: "Tim",
@@ -127,9 +122,8 @@ function App() {
     notesPreference: "GoodNotes",
     standupPreference: "40",
   };
-  
-  const users = [user1, user2, user3, user4];
 
+  const users = [user1, user2, user3, user4];
 
   const [selectedUser, setSelectedUser] = useState(null);
 
@@ -137,10 +131,6 @@ function App() {
     setSelectedUser(user);
   };
 
-  
-
-
-  
   const [isTimerPopupOpen, setIsTimerPopupOpen] = useState(false);
 
   const handleOpenTimerPopup = () => {
@@ -150,7 +140,6 @@ function App() {
   const handleCloseTimerPopup = () => {
     setIsTimerPopupOpen(false);
   };
-
 
   const [isNotesPopupOpen, setIsNotesPopupOpen] = useState(false);
 
@@ -162,8 +151,15 @@ function App() {
     setIsNotesPopupOpen(false);
   };
 
-
-  let changeGlobalVariable = (newValueName, newValueNot,newValueIcon, newValueHeight, newValueCal, newValueNotes, newValueStand) => {
+  let changeGlobalVariable = (
+    newValueName,
+    newValueNot,
+    newValueIcon,
+    newValueHeight,
+    newValueCal,
+    newValueNotes,
+    newValueStand
+  ) => {
     defaultNews = newValueNot;
     defaultIcon = newValueIcon;
     defaultHeight = newValueHeight;
@@ -180,8 +176,6 @@ function App() {
     setValue(defaultUser);
   };
 
-
-
   const [isReminderPopupOpen, setReminderPopupOpen] = useState(false);
 
   const handleOpenReminderPopup = () => {
@@ -191,9 +185,6 @@ function App() {
   const handleCloseReminderPopup = () => {
     setReminderPopupOpen(false);
   };
-
-
-
 
   const [isMusicPopupOpen, setIsMusicPopupOpen] = useState(false);
 
@@ -205,170 +196,168 @@ function App() {
     setIsMusicPopupOpen(false);
   };
 
-
-const [isGrayBackground, setIsGrayBackground] = useState(false);
+  const [isGrayBackground, setIsGrayBackground] = useState(false);
 
   const handleOffButtonClick = () => {
     setIsGrayBackground((prev) => !prev); // Toggle the state
   };
 
-
-
   return (
-
-  
-    
     <div className="App">
-
-     
-<div className={`parent ${isGrayBackground ? 'gray-background' : ''}`}>
-      
+      <div className={`parent ${isGrayBackground ? "gray-background" : ""}`}>
         <div class="div1">
-            <LeftVertical> </LeftVertical>
+          <LeftVertical> </LeftVertical>
         </div>
 
         <div class="div2">
-            <div class="div2-message"><TopHorizontal notifications={defaultNews} image={defaultIcon}> </TopHorizontal>
-            </div>
+          <div class="div2-message">
+            <TopHorizontal notifications={defaultNews} image={defaultIcon}>
+              {" "}
+            </TopHorizontal>
+          </div>
         </div>
-               
+
         <div class="div3">
-        <div className='welcome-message'>Welcome {defaultUser}!</div>
-        
-        <div className='info'> Wed Oct 10th, 2023   60 °F ☀️</div>
-            <div class="vertical-section">
+          <div className="welcome-message">Welcome {defaultUser}!</div>
 
-          <RightVertical sliderUser = {defaultHeight}> </RightVertical>
-            </div>
+          <div className="info"> Wed Oct 10th, 2023 60 °F ☀️</div>
+          <div class="vertical-section">
+            <RightVertical sliderUser={defaultHeight}> </RightVertical>
+          </div>
         </div>
-       
-    <div class="div4">
-    <div class="circle-pair">
-    <div className="circle">
-        <div>
-          {/* Wrap the image in a clickable button */}
-          <button onClick={handleOpenTimerPopup} className="image-button">
-            <img src={clock} alt="Button 1" />
-          </button>
-          {isTimerPopupOpen && (
-            <div className="overlay">
-              <Middle onClose={handleCloseTimerPopup} />
-            </div>
-          )}
-        </div>
-      </div>
-  
-      <div className="circle">
-      <div>
-        <button onClick={handleOpenReminderPopup} className="image-button">
-          {/* You can use your stand-up icon here */}
-          <img src={stand} alt="Stand Up Icon" />
-        </button>
-        {isReminderPopupOpen && (
-              <div className="overlay">
-                <StandUpReminder onClose={handleCloseReminderPopup} person={defaultUser} ST={defaultStand}/>
+
+        <div class="div4">
+          <div class="circle-pair">
+            <div className="circle">
+              <div>
+                {/* Wrap the image in a clickable button */}
+                <button onClick={handleOpenTimerPopup} className="image-button">
+                  <img src={clock} alt="Button 1" />
+                </button>
+                {isTimerPopupOpen && (
+                  <div className="overlay">
+                    <Middle onClose={handleCloseTimerPopup} />
+                  </div>
+                )}
               </div>
-            )}
-      </div>
-    </div>
-    </div>
+            </div>
 
-    <div class="circle-pair">
-        <div class="circle">
-        <div>
-          {/* Wrap the image in a clickable button */}
-          <button onClick={handleOpenMusicPopup} className="image-button">
-          <img src={music} alt="Music Icon" />
-        </button>
-        {isMusicPopupOpen && (
-          <div className="overlay">
-            <MusicPopup onClose={handleCloseMusicPopup} player={defaultUser}/>
+            <div className="circle">
+              <div>
+                <button
+                  onClick={handleOpenReminderPopup}
+                  className="image-button"
+                >
+                  {/* You can use your stand-up icon here */}
+                  <img src={stand} alt="Stand Up Icon" />
+                </button>
+                {isReminderPopupOpen && (
+                  <div className="overlay">
+                    <StandUpReminder
+                      onClose={handleCloseReminderPopup}
+                      person={defaultUser}
+                      ST={defaultStand}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
-        )}
-        </div>
-        </div>
 
-        <div className="circle">
-      <div>
-        <button onClick={handleOpenNotesPopup} className="image-button">
-          <img src={notes} alt="Notes" />
-        </button>
-        {isNotesPopupOpen && (
-          <div className="overlay">
-            <NotesPopup onClose={handleCloseNotesPopup} name={defaultNotes}/>
+          <div class="circle-pair">
+            <div class="circle">
+              <div>
+                {/* Wrap the image in a clickable button */}
+                <button onClick={handleOpenMusicPopup} className="image-button">
+                  <img src={music} alt="Music Icon" />
+                </button>
+                {isMusicPopupOpen && (
+                  <div className="overlay">
+                    <MusicPopup
+                      onClose={handleCloseMusicPopup}
+                      player={defaultUser}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="circle">
+              <div>
+                <button onClick={handleOpenNotesPopup} className="image-button">
+                  <img src={notes} alt="Notes" />
+                </button>
+                {isNotesPopupOpen && (
+                  <div className="overlay">
+                    <NotesPopup
+                      onClose={handleCloseNotesPopup}
+                      name={defaultNotes}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
+        </div>
+
+        <div class="div5">
+          <div class="day-schedule">
+            <img className="day-schedule-cal" src={defaultCal} />
+          </div>
+        </div>
+
+        <div className="div6">
+          <button className="off-button" onClick={handleOffButtonClick}>
+            Power
+          </button>
+          <button className="user-profile-button">
+            User Profile
+            <div className="nested-buttons">
+              {users.map((user) => (
+                <button
+                  key={user.id}
+                  className="small-button"
+                  onClick={() => {
+                    handleUserButtonClick(user);
+
+                    if (device) {
+                      if (device == selectedUser.name) {
+                        // Check if device is not empty
+                        changeGlobalVariable(
+                          selectedUser.name,
+                          selectedUser.notificationPreference,
+                          selectedUser.notificationPreferenceIcon,
+                          selectedUser.heightPreference,
+                          selectedUser.calendarPreference,
+                          selectedUser.notesPreference,
+                          selectedUser.standupPreference
+                        );
+                      }
+                    }
+                  }}
+                >
+                  {user.name}
+                </button>
+              ))}
+            </div>
+          </button>
+        </div>
+      </div>
+
+      <div className="device-selection">
+        <h2>User Mobile Device Selection</h2>
+        <div className="devices">
+          <button onClick={() => handleUserClick("Henry")}>Henry</button>
+          <button onClick={() => handleUserClick("Nancy")}>Nancy</button>
+          <button onClick={() => handleUserClick("Donald")}>Donald</button>
+          <button onClick={() => handleUserClick("Tim")}>Tim</button>
+        </div>
+        {device && (
+          <p>{device}'s Mobile Device Authenticated. Data Synced complete.</p>
         )}
       </div>
     </div>
-    </div>
-</div>
-
-
-<div class="div5">
-    <div class="day-schedule">
-        <img className = "day-schedule-cal" src={defaultCal} />
-    </div>
-</div>
-
-<div className="div6">
-<button className="off-button" onClick={handleOffButtonClick}>
-          Power
-        </button>
-        <button className="user-profile-button">User Profile
-  <div className="nested-buttons">
-    {users.map((user) => (
-      <button
-        key={user.id}
-        className="small-button"
-        onClick={() => {
-          handleUserButtonClick(user);
-
-          if (device) {
-            if (device == selectedUser.name){// Check if device is not empty
-            changeGlobalVariable(
-              selectedUser.name,
-              selectedUser.notificationPreference,
-              selectedUser.notificationPreferenceIcon,
-              selectedUser.heightPreference,
-              selectedUser.calendarPreference,
-              selectedUser.notesPreference,
-              selectedUser.standupPreference
-            );
-          }
-        }
-        }}
-      >
-        {user.name}
-      </button>
-    ))}
-  </div>
-</button>
-      
-    </div>
-    </div>
-  
-   
-    <div className='device-selection'>
-      <h2>User Device Selection</h2>
-      <div className='devices'>
-        <button onClick={() => handleUserClick('Henry')}>Henry</button>
-        <button onClick={() => handleUserClick('Nancy')}>Nancy</button>
-        <button onClick={() => handleUserClick('Donald')}>Donald</button>
-        <button onClick={() => handleUserClick('Tim')}>Tim</button>
-      </div>
-      {device && <p>{device}'s Device Authenticated. Data Synced complete.</p>}
-     
-    </div>
-  
-    </div>
-
-
-
-
   );
 }
 
 export default App;
-
-
-
